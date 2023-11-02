74c26befdd297a8145146b3ed2f90abc1f4a4eb6
Renato Urrea <usuaryrenato@hotmail.com>
2023-11-01 20:24:12 -0700

    solved url startegy for sing-out and sign-in

--- client/tws_main/lib/config/tws_url_strategy.dart
+++ client/tws_main/lib/config/tws_url_strategy.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:tws_main/config/routes/routes.dart';
 
 class TWSUrlStrategy extends PathUrlStrategy {
   @override
   void pushState(Object? state, String title, String url) {
-    replaceState(state, title, url);
+    String currentPath = getPath();
+    if (currentPath == accessRoute.path || url == accessRoute.path) replaceState(state, title, url);
   }
 }
