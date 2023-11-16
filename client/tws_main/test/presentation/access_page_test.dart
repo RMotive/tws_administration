8724d38bf43552448dfd1df514ccbca4977020e1
Renato Urrea <usuaryrenato@hotmail.com>
2023-11-15 17:06:59 -0800

    created and tested the correct load of an access page

--- client/tws_main/test/presentation/access_page_test.dart
+++ client/tws_main/test/presentation/access_page_test.dart
@@ -1,9 +1,19 @@
 import 'package:cosmos_foundation/extensions/int_extension.dart';
+import 'package:cosmos_foundation/helpers/theme.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:tws_main/constants/config/theme/theme_base.dart';
 import 'package:tws_main/main.dart';
+import 'package:tws_main/presentation/pages/access_page/access_page.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+  initTheme(
+    const TWSDarkTheme(),
+    <TWSThemeBase>[
+      const TWSDarkTheme(),
+      const TWSLightTheme(),
+    ],
+  );
 
   testWidgets(
     'access page loads',
@@ -12,15 +22,13 @@ void main() {
       await widgetTester.runAsync(
         () async {
           await widgetTester.pumpWidget(const TWSAdminApp());
-          await widgetTester.pump(120.seconds);
+          await Future<void>.delayed(5.seconds);
+          await widgetTester.pumpAndSettle();
+
+          Finder check = find.byType(AccessPage);
+
+          expect(check, findsOneWidget, reason: 'Access page not found');
         },
-      );
-      // --> Finding if the access page loads correctly as initial page.
-      Finder pageFinder = find.text('TWS Admin Services');
-      expect(
-        pageFinder,
-        findsOneWidget,
-        reason: 'Page not found',
       );
     },
   );
