4a6c480627962c9f3b6315bc425d9b6a41dfa75e
Juan Renato Urrea Ortiz <UsuaryRenato@hotmail.com>
2024-04-16 20:20:52 -0700

    now View is implemented to generate pages and general paging.

--- server/TWS Admin/Foundation/Migrations/Interfaces/IMigrationDepot.cs
+++ server/TWS Admin/Foundation/Migrations/Interfaces/IMigrationDepot.cs
@@ -1,6 +1,19 @@
 ﻿using Foundation.Migrations.Interfaces.Depot;
 
 namespace Foundation.Migrations.Interfaces;
+/// <summary>
+///     Determines how a complex <see cref="IMigrationDepot{TMigrationSet}"/> 
+///     implementation should behave.
+///     
+///     <br>
+///         A <see cref="IMigrationDepot{TMigrationSet}"/> is considered as a
+///         data repository for a specific <typeparamref name="TMigrationSet"/>.
+///         providing data, storing data, updating data, etc...
+///     </br>
+/// </summary>
+/// <typeparam name="TMigrationSet">
+///     The datasource object that the implementation handles.
+/// </typeparam>
 public interface IMigrationDepot<TMigrationSet>
-    : IMigrationDepot_Create<TMigrationSet>
+    : IMigrationDepot_View<TMigrationSet>, IMigrationDepot_Create<TMigrationSet>
     where TMigrationSet : IMigrationSet { }
--- server/TWS Admin/Server.Quality/Tests/Controllers/Q_SecurityController.cs
+++ server/TWS Admin/Server.Quality/Tests/Controllers/Q_SecurityController.cs
@@ -18,37 +18,4 @@ namespace Server.Quality.Tests.Controllers;
 /// </summary>
 public class Q_SecurityController
     : IClassFixture<WebApplicationFactory<Program>> {
-    const string Controller = "security/";
-
-    /// <summary>
-    ///     Host controllers manager.
-    /// </summary>
-    private readonly QualityHost HostManager;
-
-    private readonly AccountIdentityScheme WrongScheme;
-    private readonly AccountIdentityScheme CorrectScheme;
-
-    /// <summary>
-    ///     Configurations pre-testing.
-    /// </summary>
-    /// <param name="hostFactory"></param>
-    public Q_SecurityController(WebApplicationFactory<Program> hostFactory) {
-        HostManager = new QualityHost(hostFactory.CreateClient());
-        CorrectScheme = AccountIdentityPrivates.CorrectScheme;
-        WrongScheme = new() {
-            Identity = "something wrong",
-            Password = [.. "somethingwrong"u8],
-        };
-    }
-
-    [Fact]
-    public async void InitSession() {
-        const string Service = $"{Controller}initSession";
-
-        (HttpStatusCode StatusCode, SuccessExposure<ForeignSessionScheme>? ResponseBody) FirstFact = await HostManager.Post<AccountIdentityScheme, SuccessExposure<ForeignSessionScheme>>(Service, CorrectScheme);
-
-        Assert.Equal(HttpStatusCode.OK, FirstFact.StatusCode);
-        Assert.NotNull(FirstFact.ResponseBody);
-        Assert.True(FirstFact.ResponseBody.Estela.Wildcard);
-    }
 }
