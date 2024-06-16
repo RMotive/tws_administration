769f3af207cede5ca2cb5fac553bfe6abc985dc3
jr110 <c.d@hotmail.es>
2024-06-15 17:42:45 -0700

    upgrade packages refactor

---
+++ client/tws_main/lib/view/pages/settings/settings_page.dart
@@ -0,0 +1,16 @@
+import 'package:csm_foundation_view/csm_foundation_view.dart';
+import 'package:flutter/material.dart';
+
+class SettingsPage extends CSMPageBase {
+
+  const SettingsPage({
+    super.key,
+  });
+
+  @override
+  Widget compose(BuildContext ctx, Size window) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
